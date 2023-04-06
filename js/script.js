0a80157b2bb16003f383fe39e1748b665014be8e
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2023-04-06 17:49:46 -0500

    rewrite js and css for map

--- js/script.js
+++ js/script.js
@@ -1,27 +1,8 @@
-// Initialize and add the map
-let map;
-
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
+  function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 41.83156157047524, lng: -87.62729484374748},
+      zoom: 18
+    });
 }
 
-initMap();
+google.maps.event.addDomListener(window, 'load', initMap);
