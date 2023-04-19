8c236aacba6e507440f7e7bd9db451fd842fe8db
anhdang00 <122416016+anhdang00@users.noreply.github.com>
2023-04-18 20:35:16 -0500

    Update script.js
    add marker

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-  function initMap(){
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 41.83156157047524, lng: -87.62729484374748},
-      zoom: 18
-    });
+function initMap(){
+  var getMap = document.getElementById('map');
+  var myLocation = new google.maps.LatLng(41.83156157047524, -87.62729484374748);
+  var setMap = {
+    center: myLocation,
+    zoom: 18
+  }
+  var myMap = new google.maps.Map(getMap, setMap);
+  var marker = new google.maps.Marker({
+    position: myLocation,
+    map: myMap,
+    animation: google.maps.Animation.BOUNCE,
+    icon: 'images/sticker.png'
+  })
 }
 
 google.maps.event.addDomListener(window, 'load', initMap);
